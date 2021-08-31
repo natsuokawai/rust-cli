cc0bcfae88852e698769186754f92806aaa6d66a
natsuokawai <kawai.natsuo@gmail.com>
2021-08-31 19:35:19 +0900

    cat: check the number of args

--- cat/src/main.rs
+++ cat/src/main.rs
@@ -1,6 +1,12 @@
 use std::env;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args[1]);
+    let filename = &args[1..];
+    if filename.len() == 0 {
+        eprintln!("{}: file name not given.", args[0]);
+        process::exit(1);
+    }
+    println!("{:?}", filename);
 }
