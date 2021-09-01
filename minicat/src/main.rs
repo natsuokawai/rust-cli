236bb355d25eac4eababf2db01002322a8374a6b
natsuokawai <kawai.natsuo@gmail.com>
2021-09-02 07:59:26 +0900

    minicat: modify main function

--- minicat/src/main.rs
+++ minicat/src/main.rs
@@ -1,12 +1,17 @@
+extern crate minicat;
+
 use std::env;
+use std::io;
 use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let filename = &args[1..];
-    if filename.len() == 0 {
+    if args.len() == 1 {
         eprintln!("{}: file name not given.", args[0]);
         process::exit(1);
     }
-    println!("{:?}", filename);
+    let filenames: Vec<String> = args[1..].to_vec();
+    let stdout = io::stdout();
+    let mut stdout = stdout.lock();
+    minicat::run(&mut stdout, filenames).expect("failed to cat");
 }
